1605babfdd4ecabc2860e95309169c98c99d4f69
Piotr Pesta <piotr.pesta@gmail.com>
2016-12-07 22:23:50 +0100

    v0.2.3
    *removed zero days after current day
    *minor changes

--- classes.php
+++ classes.php
@@ -54,17 +54,25 @@ class Advanced_Dashboard_View { # Dashboard view
         $advanced_dashboard_call_month = Advanced_Dashboard_Call_And_Chart::advanced_dashboard_call('month'); # Month interval
         $advanced_dashboard_call_day = Advanced_Dashboard_Call_And_Chart::advanced_dashboard_call('day'); # 1 Day interval
         ?>
-        <form id="form1">
+        <form action="" id="form1" method="post">
             <input type="hidden" id="gifDir" value="<?php echo plugins_url('cat_loading.gif', __FILE__) ?>" />
-            <input type="date" name="startdate" class="datepicker" id="startdate" />
-            <input type="date" name="enddate" class="datepicker" id="enddate" />
-            <input type="button" onclick="change_data_ad()" value="Apply" id="submitdates" />
+            <input type="date" name="startdate" class="datepicker" name="startdate" id="startdate" />
+            <input type="date" name="enddate" class="datepicker" name="enddate" id="enddate" />
+            <input type="submit" value="Apply" name="submitdates" id="submitdates" />
+            <?php
+            if (isset($_POST['startdate']) && isset($_POST['enddate'])) {
+                ?>
+                <input type="hidden" id="start" value="<?php echo $_POST['startdate'] ?>" />
+                <input type="hidden" id="end" value="<?php echo $_POST['enddate'] ?>" />
+                <?php
+            }
+            ?>
         </form>
         <ul id="advanced-dashboard-ul">
             <li>
                 <div><?php echo Advanced_Dashboard_Call_And_Chart::advanced_dashboard_chart1_view(); ?></div>
                 <div><?php printf(__("<strong>%s</strong> net sales this month", 'woocommerce'), wc_price($advanced_dashboard_call_month[1]->net_sales)); ?></div>
-                <?php printf(__("<strong>%s</strong> net sales this day", 'woocommerce'), wc_price($advanced_dashboard_call_day[1]->net_sales)); ?>
+                <?php printf("<strong>%s</strong> - " . __("Net Sales amount", 'woocommerce'), wc_price($advanced_dashboard_call_day[1]->net_sales)); ?>
             </li>
             <li>
                 <div><?php echo Advanced_Dashboard_Call_And_Chart::advanced_dashboard_chart2_view(); ?></div>
@@ -97,19 +105,37 @@ class Advanced_Dashboard_View { # Dashboard view
         <?php
     }
 
+    public static function advanced_dashboard_dates() { # For dates form
+        if (isset($_POST['startdate']) && isset($_POST['enddate'])) {
+            $dates = array();
+            $dates[0] = strtotime($_POST['startdate']);
+            $dates[1] = strtotime($_POST['enddate']);
+            return $dates;
+        }
+    }
+
 }
 
+$dupsko = new Advanced_Dashboard_Call_And_Chart();
+
 class Advanced_Dashboard_Call_And_Chart {
+
+    public static $Currentdate;
 
     public static function advanced_dashboard_call($interval) { # Returns SQL data, sales value, data, number of orders
         $reports = new WC_Admin_Report();
         $sales_by_date = new WC_Report_Sales_By_Date();
-        if ($interval == 'month') {
+        $dates = Advanced_Dashboard_View::advanced_dashboard_dates();
+        if ($interval == 'month' && !$dates) {
             $sales_by_date->start_date = strtotime(date('Y-m-01', current_time('timestamp')));
+            $sales_by_date->end_date = current_time('timestamp');
         } elseif ($interval == 'day') {
             $sales_by_date->start_date = current_time('timestamp');
+            $sales_by_date->end_date = current_time('timestamp');
+        } elseif ($dates) {
+            $sales_by_date->start_date = $dates[0];
+            $sales_by_date->end_date = $dates[1];
         }
-        $sales_by_date->end_date = current_time('timestamp');
         $sales_by_date->chart_groupby = 'day';
         $sales_by_date->group_by_query = 'YEAR(posts.post_date), MONTH(posts.post_date), DAY(posts.post_date)';
         $report_data = $sales_by_date->get_report_data();
@@ -145,6 +171,11 @@ class Advanced_Dashboard_Call_And_Chart {
 
         foreach ($JStable as $JSdata) {
             $valueFormat = DateTime::createFromFormat('Y/m/j', $JSdata['date']);
+            $Comparedate = $valueFormat->format('Y/m/j');
+            self::$Currentdate = date('Y/m/j', time());
+            if ($Comparedate > self::$Currentdate) {
+                break;
+            }
             $year = $valueFormat->format('Y');
             $month = $valueFormat->format('m') - 1;
             $day = $valueFormat->format('j');
@@ -173,9 +204,12 @@ class Advanced_Dashboard_Call_And_Chart {
         }
 
         $JStable = self::advanced_dashboard_zero_days($numberOfDays, $JStableValue, $formatDate, $type = 'qty'); # Add days with no orders to chart
-        //print_r($JStable);
         foreach ($JStable as $JSdataQty) {
             $formatDate = DateTime::createFromFormat('Y/m/d', $JSdataQty['date']);
+            $Comparedate = $formatDate->format('Y/m/j');
+            if ($Comparedate > self::$Currentdate) {
+                break;
+            }
             $year = $formatDate->format('Y');
             $month = $formatDate->format('m') - 1;
             $day = $formatDate->format('j');
@@ -184,8 +218,17 @@ class Advanced_Dashboard_Call_And_Chart {
         }
     }
 
-    public static function advanced_dashboard_sql_qty() { # SQL data miner
+    public static function advanced_dashboard_sql_qty() { # SQL data miner quantity
         global $wpdb;
+
+        $dates = Advanced_Dashboard_View::advanced_dashboard_dates();
+        if ($dates) {
+            $startDateSQL = date('Y-m-d', $dates[0]);
+            $endDateSQL = date('Y-m-d H:i:s', $dates[1]);
+        } else {
+            $startDateSQL = date('Y-m-01', current_time('timestamp'));
+            $endDateSQL = date('Y-m-d H:i:s', current_time('timestamp'));
+        }
 
         $query = array();
         $query['fields'] = "SELECT SUM({$wpdb->prefix}woocommerce_order_itemmeta.meta_value) AS qty, DATE({$wpdb->prefix}posts.post_date) AS date
@@ -194,8 +237,8 @@ class Advanced_Dashboard_Call_And_Chart {
         $query['join'] .= "INNER JOIN {$wpdb->prefix}posts ON {$wpdb->prefix}woocommerce_order_items.order_id = {$wpdb->prefix}posts.ID ";
         $query['where'] = "WHERE {$wpdb->prefix}posts.post_status IN ( 'wc-" . implode("','wc-", array('completed', 'processing', 'on-hold')) . "' ) ";
         $query['where'] = "AND {$wpdb->prefix}woocommerce_order_itemmeta.meta_key = '_qty' ";
-        $query['where'] .= "AND {$wpdb->prefix}posts.post_date >= '" . date('Y-m-01', current_time('timestamp')) . "' ";
-        $query['where'] .= "AND {$wpdb->prefix}posts.post_date <= '" . date('Y-m-d H:i:s', current_time('timestamp')) . "' ";
+        $query['where'] .= "AND {$wpdb->prefix}posts.post_date >= '" . $startDateSQL . "' ";
+        $query['where'] .= "AND {$wpdb->prefix}posts.post_date <= '" . $endDateSQL . "' ";
         $query['groupby'] = "GROUP BY date ";
         $query['orderby'] = "ORDER BY date ASC";
 
@@ -277,47 +320,51 @@ class Advanced_Dashboard_Chart_Scripts { # Google Charts JS
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
-                google.charts.load('current', {'packages': ['corechart', 'line']});
-                google.charts.setOnLoadCallback(drawChart);
+            google.charts.load('current', {'packages': ['corechart', 'line']});
+            google.charts.setOnLoadCallback(drawChart);
 
-                function drawChart() {
-                    var data = new google.visualization.DataTable();
-                    data.addColumn('date', 'Date');
-                    data.addColumn('number', 'Sales');
-                    data.addColumn('number', 'Orders');
+            function drawChart() {
+                var data = new google.visualization.DataTable();
+                data.addColumn('date', 'Date');
+                data.addColumn('number', 'Sales Value');
+                data.addColumn('number', 'Orders');
 
-                    data.addRows([
+                data.addRows([
         <?php
         $advanced_dashboard_call_month = Advanced_Dashboard_Call_And_Chart::advanced_dashboard_call('month');
         if ($advanced_dashboard_call_month[1]->orders[0]->post_date) { # If there is any orders start data loop
             Advanced_Dashboard_Call_And_Chart::advanced_dashboard_chart_loop_value_orders();
         }
         ?>
-                    ]);
-                    var options = {
-                        series: {
-                            0: {targetAxisIndex: 0, lineWidth: 5, color: '#FFD700'},
-                            1: {targetAxisIndex: 1}
+                ]);
+                var options = {
+                    series: {
+                        0: {targetAxisIndex: 0, lineWidth: 5, color: '#FFD700'},
+                        1: {targetAxisIndex: 1}
+                    },
+                    hAxis: {
+                        title: 'Time'
+                    },
+                    vAxes: {
+                        0: {
+                            textPosition: 'none'
                         },
-                        hAxis: {
-                            title: 'Time'
-                        },
-                        vAxis: {
-                            0: {title: 'Value'},
-                            1: {title: 'Orders'}
-                        },
-                        title: 'This Month Sales and Orders',
-                        legend: {position: 'bottom'}
-                    };
+                        1: {
+                            textPosition: 'none'
+                        }
+                    },
+                    title: 'This Month Sales and Orders',
+                    legend: {position: 'bottom'}
+                };
 
-                    var formatter = new google.visualization.NumberFormat(
-                            {pattern: '###,###', suffix: " zł"});
-                    formatter.format(data, 1);
+                var formatter = new google.visualization.NumberFormat(
+                        {pattern: '###,###', suffix: " <?php echo html_entity_decode(get_woocommerce_currency_symbol(), ENT_QUOTES, 'UTF-8'); ?>"});
+                formatter.format(data, 1);
 
-                    var chart1 = new google.visualization.LineChart(document.getElementById('chart1'));
+                var chart1 = new google.visualization.LineChart(document.getElementById('chart1'));
 
-                    chart1.draw(data, options);
-                }
+                chart1.draw(data, options);
+            }
         </script>
 
         <?php
@@ -333,7 +380,7 @@ class Advanced_Dashboard_Chart_Scripts { # Google Charts JS
             function drawChart() {
                 var data2 = new google.visualization.DataTable();
                 data2.addColumn('date', 'Date');
-                data2.addColumn('number', 'Qty');
+                data2.addColumn('number', 'Qty Sold');
 
                 data2.addRows([
         <?php
@@ -367,7 +414,7 @@ class Advanced_Dashboard_Chart_Scripts { # Google Charts JS
 
     public static function advanced_dashboard_chart_custom_script() { # Custom script to header
         wp_enqueue_script('jquery-ui-datepicker');
-        wp_register_style('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css');
+        wp_register_style('jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css');
         wp_enqueue_style('jquery-ui');
         wp_enqueue_script('custom-js', plugins_url('scripts.js', __FILE__));
     }
